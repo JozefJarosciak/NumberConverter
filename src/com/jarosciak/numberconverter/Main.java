97f7d80d3abdbc2e0528abffee6a8f9c2b277278
jarosciak@gmail.com <Gith1974>
2018-01-27 15:12:33 -0500

    fix

--- src/com/jarosciak/numberconverter/Main.java
+++ src/com/jarosciak/numberconverter/Main.java
@@ -1,14 +1,24 @@
 package com.jarosciak.numberconverter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
-    // initialize global scanner for input from command-line
-    private static Scanner scanner = new Scanner(System.in);
-    private static String appointmentArray[][];
+    public static void main(String[] args) throws ParseException {
 
-    public static void main(String[] args) {
+        // initialize scanner for input from command-line
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> startArrayList = new ArrayList<String>();
+        ArrayList<String> endArrayList = new ArrayList<String>();
+        ArrayList<String> descriptionArrayList = new ArrayList<String>();
+        ArrayList<String> differencesArrayList = new ArrayList<String>();
+
 
         // infinite loop for asking questions
         while (true) {
@@ -17,94 +27,66 @@ public class Main {
             String addAppointment = scanner.next();
 
             if (addAppointment.toLowerCase().equals("yes")) {
-                // keep asking questions while answer is 'yes'
-                AddAppointments();
+                // get start time and add it to its array list
+                System.out.print("Enter starting time: ");
+                startArrayList.add(scanner.next());
+
+                // get end time and add it to its array list
+                System.out.print("Enter ending time: ");
+                endArrayList.add(scanner.next());
+
+                // get appointment description and add it to its array list
+                System.out.print("Enter appointment description: ");
+                descriptionArrayList.add(scanner.next());
+
+                // separator blank line
+                System.out.println();
+
             } else {
                 // If the answer is not 'yes', print the results
-                System.out.println("");
+
+                System.out.println("        OUTPUT      ");
                 System.out.println("----------------------");
-                System.out.println("You can take longest nap at 16:00 and it will last for 30 minutes.\n" +
-                        "Total free time during whole day is 30 minutes.");
-                // end the while
+
+
+                for(int i = 0; i < startArrayList.size(); i++) {
+                    // calculate the difference between time (spaces between appointments)
+                    SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+                    Date date1 = format.parse(endArrayList.get(i));
+
+                    if (i==0) {
+                        Date date2 = format.parse(startArrayList.get(i));
+                        Date date3 = format.parse("9:00");
+                        long difference = (date2.getTime() - date3.getTime()) / 1000; // difference in seconds
+                        differencesArrayList.add(Long.toString(difference) + " - " + "9:00" );
+                    }
+
+                    if (i < (startArrayList.size()-1)) {
+                        Date date2 = format.parse(startArrayList.get(i + 1));
+                        long difference = (date2.getTime() - date1.getTime()) / 1000; // difference in seconds
+                        differencesArrayList.add(Long.toString(difference)  + " - " +  endArrayList.get(i));
+                    } else {
+                        Date date2 = format.parse("17:00");
+                        long difference = (date2.getTime() - date1.getTime()) / 1000; // difference in seconds
+                        differencesArrayList.add(Long.toString(difference) + " - " + endArrayList.get(i));
+                    }
+                }
+
+               Collections.sort(differencesArrayList);
+
+                for (int i = 0; i < differencesArrayList.size(); i++) {
+                    System.out.println(differencesArrayList.get(i));
+                }
+
+
+
                 break;
             }
         }
-
-
     }
 
 
 
 
 
-    private static void AddAppointments() {
-        // get starting time
-        System.out.print("Enter starting time: ");
-        String startTime = scanner.next();
-
-        // get ending time
-        System.out.print("Enter ending time: ");
-        String endTime = scanner.next();
-
-        // get appointment description
-        System.out.print("Enter appointment description: ");
-        String appointmentDescription = scanner.next();
-
-        // separator blank line
-        System.out.print(" ");
-
-        // add values to object
-        Appointments appointment = new Appointments();
-        appointment.setStartTime(startTime);
-        appointment.setEndTime(endTime);
-        appointment.setAppointmentDescription(appointmentDescription);
-
-        System.out.println(appointment.toString());
-
-     }
-
-}
-
-
-class Appointments {
-    // data members
-    private String appointmentDescription;
-    private String startTime;
-    private String endTime;
-
-    // Set Appointment Description
-    public void setAppointmentDescription(String data)
-    {
-        appointmentDescription = data;
-    }
-
-    // Set Start Time
-    public void setStartTime(String data)
-    {
-        startTime = data;
-    }
-
-    // Set End Time
-    public void setEndTime(String data)
-    {
-        endTime = data;
-    }
-
-    // Get Appointment Description
-    public String getAppointmentDescription()
-    {
-        return appointmentDescription;
-    }
-
-    // Get Start Time
-    public String getStartTime()
-    {
-        return startTime;
-    }
-
-    // Get End Time
-    public String setEndTime()
-    {
-        return endTime;
-    }
 }
