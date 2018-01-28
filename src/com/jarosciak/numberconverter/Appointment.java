9c6d1c666faaf30f0f633788ac12ddd47793dec0
jarosciak@gmail.com <Gith1974>
2018-01-27 22:59:59 -0500

    fix

---
+++ src/com/jarosciak/numberconverter/Appointment.java
@@ -0,0 +1,56 @@
+package com.jarosciak.numberconverter;
+
+public class Appointment {
+
+    // data members
+    private String appointmentDescription;
+    private String startTime;
+    private String endTime;
+    private Long durationTime;
+
+    // Set Appointment Description
+    public void setAppointmentDescription(String data)
+    {
+        appointmentDescription = data;
+    }
+
+    // Set Start Time
+    public void setStartTime(String data)
+    {
+        startTime = data;
+    }
+
+    // Set End Time
+    public void setEndTime(String data)
+    {
+        endTime = data;
+    }
+
+    // Set Duration
+    public void setDurationTime(Long durationTime) {
+        this.durationTime = durationTime;
+    }
+
+    // Get Appointment Description
+    public String getAppointmentDescription()
+    {
+        return appointmentDescription;
+    }
+
+    // Get Start Time
+    public String getStartTime()
+    {
+        return startTime;
+    }
+
+    // Get End Time
+    public String getEndTime()
+    {
+        return endTime;
+    }
+
+    // Get Duration
+    public Long getDurationTime() {
+        return durationTime;
+    }
+}
--- src/com/jarosciak/numberconverter/Main.java
+++ src/com/jarosciak/numberconverter/Main.java
@@ -2,11 +2,11 @@ package com.jarosciak.numberconverter;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -14,11 +14,10 @@ public class Main {
 
         // initialize scanner for input from command-line
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> startArrayList = new ArrayList<String>();
-        ArrayList<String> endArrayList = new ArrayList<String>();
-        ArrayList<String> descriptionArrayList = new ArrayList<String>();
         ArrayList<String> differencesArrayList = new ArrayList<String>();
 
+        ArrayList<Appointment> arrayOfAppointments = new ArrayList<>();
+        Appointment app = new Appointment();
 
         // infinite loop for asking questions
         while (true) {
@@ -29,35 +28,46 @@ public class Main {
             if (addAppointment.toLowerCase().equals("yes")) {
                 // get start time and add it to its array list
                 System.out.print("Enter starting time: ");
-                startArrayList.add(scanner.next());
+                String start = scanner.next();
+                app.setStartTime(start);
 
                 // get end time and add it to its array list
                 System.out.print("Enter ending time: ");
-                endArrayList.add(scanner.next());
+                String end = scanner.next();
+                app.setEndTime(end);
 
                 // get appointment description and add it to its array list
                 System.out.print("Enter appointment description: ");
-                descriptionArrayList.add(scanner.next());
+                String description = scanner.next();
+                app.setAppointmentDescription(description);
+
+
+                SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+                long difference = (format.parse(end).getTime() - format.parse(start).getTime()); // difference in second
+                app.setDurationTime(difference);
+
+                // add the object to array list
+                arrayOfAppointments.add(app);
 
                 // separator blank line
-                System.out.println();
+                //System.out.println(arrayOfAppointments.get(0).getStartTime());
 
             } else {
                 // If the answer is not 'yes', print the results
 
-                System.out.println("        OUTPUT      ");
+                System.out.println("----------------------");
+                System.out.println("|        OUTPUT      |");
                 System.out.println("----------------------");
 
+                /*
 
-                for(int i = 0; i < startArrayList.size(); i++) {
+                for(int i = 0; i < arrayOfAppointments.size(); i++) {
                     // calculate the difference between time (spaces between appointments)
                     SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-                    Date date1 = format.parse(endArrayList.get(i));
+                    Date date1 = format.parse(arrayOfAppointments.get(i).getStartTime());
 
                     if (i==0) {
-                        Date date2 = format.parse(startArrayList.get(i));
-                        Date date3 = format.parse("9:00");
-                        long difference = (date2.getTime() - date3.getTime()) / 1000; // difference in seconds
+                        long difference = (format.parse(arrayOfAppointments.get(i).getEndTime()).getTime() - format.parse("9:00").getTime()) / 1000; // difference in seconds
                         differencesArrayList.add(Long.toString(difference) + " - " + "9:00" );
                     }
 
@@ -73,9 +83,28 @@ public class Main {
                 }
 
                Collections.sort(differencesArrayList);
+*/
 
-                for (int i = 0; i < differencesArrayList.size(); i++) {
-                    System.out.println(differencesArrayList.get(i));
+
+
+                // Get Total Free Time
+                long sum = 8*3600000; // milliseconds in one day from 9:00 to 17:00
+                for (int i = 0; i < arrayOfAppointments.size(); i++) {
+                    sum -= arrayOfAppointments.get(i).getDurationTime();
+                }
+
+                System.out.println(sum);
+
+                int hours = Integer.parseInt(String.format("%02d", TimeUnit.MILLISECONDS.toHours(sum)));
+
+                int minutes = Integer.parseInt(String.format("%02d",TimeUnit.MILLISECONDS.toMinutes(sum) -
+                                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(sum))));
+
+
+                if (hours>0) {
+                    System.out.println("Total free time during whole day is " + hours + " hours and " + minutes + " minutes.");
+               } else {
+                    System.out.println("Total free time during whole day is " + minutes + " minutes.");
                 }
 
 
