c7cf58117fbbba57762d51806846a2c5ccbbe0ee
jarosciak@gmail.com <Gith1974>
2018-01-28 09:52:28 -0500

    fix

--- src/com/jarosciak/numberconverter/Main.java
+++ src/com/jarosciak/numberconverter/Main.java
@@ -2,10 +2,7 @@ package com.jarosciak.numberconverter;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -14,40 +11,39 @@ public class Main {
 
         // initialize scanner for input from command-line
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> differencesArrayList = new ArrayList<String>();
-
         ArrayList<Appointment> arrayOfAppointments = new ArrayList<>();
-        Appointment app = new Appointment();
+
 
         // infinite loop for asking questions
         while (true) {
             // Add next appointment if answer is YES
             System.out.print("Do you want to add an appointment? ");
             String addAppointment = scanner.next();
+            Appointment meeting = new Appointment();
 
             if (addAppointment.toLowerCase().equals("yes")) {
                 // get start time and add it to its array list
                 System.out.print("Enter starting time: ");
                 String start = scanner.next();
-                app.setStartTime(start);
+                meeting.setStartTime(start);
 
                 // get end time and add it to its array list
                 System.out.print("Enter ending time: ");
                 String end = scanner.next();
-                app.setEndTime(end);
+                meeting.setEndTime(end);
 
                 // get appointment description and add it to its array list
                 System.out.print("Enter appointment description: ");
                 String description = scanner.next();
-                app.setAppointmentDescription(description);
+                meeting.setAppointmentDescription(description);
 
 
                 SimpleDateFormat format = new SimpleDateFormat("HH:mm");
                 long difference = (format.parse(end).getTime() - format.parse(start).getTime()); // difference in second
-                app.setDurationTime(difference);
+                meeting.setDurationTime(difference);
 
                 // add the object to array list
-                arrayOfAppointments.add(app);
+                arrayOfAppointments.add(meeting);
 
                 // separator blank line
                 //System.out.println(arrayOfAppointments.get(0).getStartTime());
@@ -59,48 +55,66 @@ public class Main {
                 System.out.println("|        OUTPUT      |");
                 System.out.println("----------------------");
 
-                /*
+
+                   Map<String,Long> durationTime = new HashMap<String,Long>();
 
                 for(int i = 0; i < arrayOfAppointments.size(); i++) {
                     // calculate the difference between time (spaces between appointments)
                     SimpleDateFormat format = new SimpleDateFormat("HH:mm");
-                    Date date1 = format.parse(arrayOfAppointments.get(i).getStartTime());
 
                     if (i==0) {
-                        long difference = (format.parse(arrayOfAppointments.get(i).getEndTime()).getTime() - format.parse("9:00").getTime()) / 1000; // difference in seconds
-                        differencesArrayList.add(Long.toString(difference) + " - " + "9:00" );
+                        // 9:00 calculation
+                        long difference = (format.parse(arrayOfAppointments.get(i).getStartTime()).getTime() - format.parse("9:00").getTime());
+                        durationTime.put("9:00",difference);
                     }
 
-                    if (i < (startArrayList.size()-1)) {
-                        Date date2 = format.parse(startArrayList.get(i + 1));
-                        long difference = (date2.getTime() - date1.getTime()) / 1000; // difference in seconds
-                        differencesArrayList.add(Long.toString(difference)  + " - " +  endArrayList.get(i));
+                    if (i < (arrayOfAppointments.size()-1)) {
+                        // after 9:00 before 17:00 calculation
+                        long difference = (format.parse(arrayOfAppointments.get(i + 1).getStartTime()).getTime() - format.parse(arrayOfAppointments.get(i).getEndTime()).getTime());
+                        durationTime.put(arrayOfAppointments.get(i).getEndTime(),difference);
                     } else {
-                        Date date2 = format.parse("17:00");
-                        long difference = (date2.getTime() - date1.getTime()) / 1000; // difference in seconds
-                        differencesArrayList.add(Long.toString(difference) + " - " + endArrayList.get(i));
+                        // 17:00 calculation
+                        long difference = (format.parse("17:00").getTime() - format.parse(arrayOfAppointments.get(i).getEndTime()).getTime());
+                        durationTime.put(arrayOfAppointments.get(i).getEndTime(),difference);
                     }
                 }
 
-               Collections.sort(differencesArrayList);
-*/
+                // get max nap duration from Map
+                Long longestNapDuration = durationTime.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getValue();
+                String longestNapTime = durationTime.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
+
+           //     System.out.println("Longest: " + differencesArrayList.get(differencesArrayList.size()));
+
+
+
+
+
+                int napHours = Integer.parseInt(String.format("%02d", TimeUnit.MILLISECONDS.toHours(longestNapDuration)));
+                int napMinutes = Integer.parseInt(String.format("%02d",TimeUnit.MILLISECONDS.toMinutes(longestNapDuration)
+                        - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(longestNapDuration))));
+
+                // format answer
+                if (napHours>0) {
+                    System.out.println("You can take longest nap at " + longestNapTime + " and it will last for " + napHours + " hours and " + napMinutes + " minutes." );
+                } else {
+                    System.out.println("You can take longest nap at " + longestNapTime + " and it will last for " + napMinutes + " minutes." );
+                }
+
+
 
 
 
                 // Get Total Free Time
-                long sum = 8*3600000; // milliseconds in one day from 9:00 to 17:00
+                long sum = 8*60*60*1000; // milliseconds in one day from 9:00 to 17:00
                 for (int i = 0; i < arrayOfAppointments.size(); i++) {
                     sum -= arrayOfAppointments.get(i).getDurationTime();
                 }
 
-                System.out.println(sum);
-
                 int hours = Integer.parseInt(String.format("%02d", TimeUnit.MILLISECONDS.toHours(sum)));
-
-                int minutes = Integer.parseInt(String.format("%02d",TimeUnit.MILLISECONDS.toMinutes(sum) -
-                                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(sum))));
+                int minutes = Integer.parseInt(String.format("%02d",TimeUnit.MILLISECONDS.toMinutes(sum) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(sum))));
 
 
+                // format answer
                 if (hours>0) {
                     System.out.println("Total free time during whole day is " + hours + " hours and " + minutes + " minutes.");
                } else {
